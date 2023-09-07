f6b0c4993fa435b2bc7e1b9cad6fc9f70fe0d8b4
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2023-09-08 03:28:57 +0530

    Add files via upload

---
+++ kickstart with nodejs/destructring.js
@@ -0,0 +1,28 @@
+/*
+const student = {
+    name: 'Alok Shakya',
+    age: 24,
+    greet() {
+        console.log('hi' + this.name)
+    }
+}
+
+
+const printDetails = ({ name, age }) => {
+    console.log(name, age)
+}
+
+printDetails(student)
+
+const { name, greet } = student;
+console.log(name, greet)
+
+*/
+
+
+
+
+// for array
+const hob = ['sports', 'cooking', 'books'];
+const [hob1, hob2] = hob;
+console.log(hob1, hob2)
