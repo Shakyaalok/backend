676da91a20b90515c0a18def2e9bc8dc0b228a75
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2023-09-29 16:25:46 +0530

    Add files via upload

---
+++ Expense Tracker - Node.js Project/pr_04_SingUp_hash_login_compare/controllers/signupControllers.js
@@ -0,0 +1,94 @@
+const signupModel = require('../models/singupModels');
+const bcrypt = require('bcrypt')
+
+
+const singUpUser = async(req, res) => {
+
+    const { name, email, mobile, password } = req.body;
+
+
+
+
+    if (!name) {
+        return res.status(500).json('Name is Required')
+    }
+    if (!email) {
+        return res.status(500).json('Email is Required')
+    }
+    if (!password) {
+        return res.status(500).json('Password is Required')
+    }
+    if (!mobile) {
+        return res.status(500).json('Phone Number is Required')
+    }
+
+    const existingEmail = await signupModel.findOne({ where: { email } })
+
+
+    if (existingEmail) {
+        return res.status(200).json({
+            success: true,
+            message: "Already registered please login"
+        })
+    }
+
+    // hasing
+    const saltRounds = 12;
+    bcrypt.hash(password, saltRounds, async(err, hash) => {
+        console.log(err);
+
+        // then saving
+        const user = await signupModel.create({ name, email, mobile, password: hash })
+        res.status(201).json({
+            success: true,
+            message: "Register successfully!",
+            user
+        })
+    })
+
+
+
+}
+
+// login
+const login = async(req, res) => {
+    const { email, password } = req.body;
+    const user = await signupModel.findOne({ where: { email } });
+
+
+    if (!user) {
+        return res.status(200).json({
+            success: false,
+            message: 'Email id not regsiter'
+        })
+    }
+
+    const existingpassword = await signupModel.findOne({ where: { password } });
+    bcrypt.compare(password, existingpassword, function(err, result) {
+
+        if (!err) {
+            return res.status(200).json({
+                success: false,
+                message: 'Creditentials does not match'
+            })
+        }
+
+
+        res.status(201).json({
+            success: true,
+            message: 'Login Successfully!',
+            user: {
+                name: user.name
+            }
+        })
+
+    })
+
+
+
+
+
+}
+
+
+module.exports = { singUpUser, login }
---
+++ Expense Tracker - Node.js Project/pr_04_SingUp_hash_login_compare/public/index.js
@@ -0,0 +1,86 @@
+// const btn = document.getElementById('btn')
+const loginbtn = document.getElementById('login-btn')
+
+// btn.addEventListener('click', singUp);
+loginbtn.addEventListener('click', login)
+
+
+function singUp() {
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const mobile = document.getElementById('mobile').value
+    const password = document.getElementById('password').value
+
+
+
+    const user = {
+        name: name,
+        email: email,
+        mobile: mobile,
+        password: password
+    }
+
+    axios.post('http://localhost:4000/singup', user)
+        .then((response) => {
+
+            console.log(response)
+            if (response.status === 201) {
+
+                showMsg(response.data.message)
+                console.log(response.data.message)
+
+            } else if (response.status === 200) {
+
+                const errorMessage = response.data.message;
+                showMsg(errorMessage)
+
+            }
+        }).catch((err) => console.log(err))
+}
+
+
+
+
+function login() {
+    console.log(1)
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+
+
+    const crederntials = {
+        email: email,
+        password: password
+    }
+
+
+
+
+    axios.post('http://localhost:4000/singup/login', crederntials)
+        .then((response) => {
+
+            console.log(response)
+            if (response.status === 201) {
+
+                showMsg('Welcome back ' + response.data.user.name)
+                console.log(response.data.message)
+
+            } else if (response.status === 200) {
+
+                const errorMessage = response.data.message;
+                showMsg(errorMessage)
+
+            }
+        }).catch((err) => console.log(err))
+}
+
+
+function showMsg(message) {
+    const getMessage = document.getElementById('showMsg')
+    const newSpan = document.createElement('span')
+    newSpan.innerHTML = `
+    <span>${message}</span>
+    `
+
+    getMessage.appendChild(newSpan)
+}
