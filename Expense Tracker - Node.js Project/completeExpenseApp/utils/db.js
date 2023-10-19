1745f81d247468e644198f85d275d8f98d2fb0ce
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2023-10-19 10:38:03 +0530

    Delete Expense Tracker - Node.js Project/completeExpenseApp directory

--- Expense Tracker - Node.js Project/completeExpenseApp/utils/db.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('expense_app', 'root', 'alokshakya', {
-    host: 'localhost',
-    dialect: 'mysql' /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
-});
-
-
-
-module.exports = sequelize;
