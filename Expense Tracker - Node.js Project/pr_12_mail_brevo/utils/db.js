b5caa1a05316a62d0c3fdd31f244a8f25285cf2b
Shakyaalok <108414896+Shakyaalok@users.noreply.github.com>
2023-10-24 06:09:22 +0530

    Delete Expense Tracker - Node.js Project/pr_12_mail_brevo directory

--- Expense Tracker - Node.js Project/pr_12_mail_brevo/utils/db.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('signup', 'root', 'alokshakya', {
-    host: 'localhost',
-    dialect: 'mysql'
-})
-
-
-module.exports = sequelize
